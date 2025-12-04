78e94ef1d09350f4fd30cdda6eb44173823f4049
riofadillah <riofadillah19@student.upi.edu>
2025-12-04 12:38:41 +0700

    first commit

---
+++ fungsi.h
@@ -0,0 +1,19 @@
+#ifndef FUNGSI_H
+#define FUNGSI_H
+
+void menuMulai();
+void tambahPeserta();
+void acakLapak();
+void menuLihatPeserta();
+void lihatPeserta();
+void cariPeserta();
+void sortDataNama();
+void sortDataLapak();
+void kantin();
+void pembayaran();
+void hasilIkan();
+void pemenang();
+void akhiriSesi();
+void menuRiwayat();
+
+#endif
